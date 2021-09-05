47ba13d7a026698efdacb28e1bff0a5a19c17a79
Ashish Gautam <agautam@hamropatro.com>
2021-09-05 07:50:47 +0545

    project setup with webpack, eslint, stylelint, and prettier

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import HomePage from "./pages/home";
+
+ReactDOM.render(<HomePage />, document.getElementById("app-root"));
